766c238495141d3531a17a4ccff63e213f0a6e77
Blank Spruce <32396809+BlankSpruce@users.noreply.github.com>
2019-12-18 22:17:17 +0100

    Reworked bracket_argument and added tests for corner cases

--- tests/inputs/bracket_arguments.cmake
+++ tests/inputs/bracket_arguments.cmake
@@ -3,6 +3,18 @@ message(first [=[multi
 line bracket
 argument]=] third)
 
+# Some other symbol than = after one of inner ]
+foo(bar [[baz [0-9]+]])
+
+# Multiple = but not matching number
+foo([====[some content
+    with too few equal signs ]] ]=] ]==] ]===]] and
+    too many ]=====] ]======] ]=======] and
+    incomplete ]==== bracket_close ]====])
+    
+foo(bar [=====[some content with ]==== ]=====])
+
+
 # Taken from CMake documentation
 message([=[
 This is the first line in a bracket argument with bracket length 1.
