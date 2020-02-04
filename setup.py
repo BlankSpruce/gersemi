7900cfb5dfe943477f52648ae58f9b9829c4036e
Blank Spruce <32396809+BlankSpruce@users.noreply.github.com>
2020-02-04 17:13:22 +0100

    Fixed packages inclusion in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="gersemi",
     version="0.1.0",
-    packages=["gersemi"],
+    packages=find_packages(include=["gersemi", "gersemi.*"]),
     package_data={"gersemi": ["cmake.lark"]},
     entry_points={"console_scripts": ["gersemi = gersemi.__main__:main"]},
     install_requires=["lark-parser==0.8"],
