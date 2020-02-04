2bc338aba519a848f9a46a64a7570f3e24a0bb5d
Blank Spruce <32396809+BlankSpruce@users.noreply.github.com>
2020-02-04 21:13:25 +0100

    Downcasing command names

--- gersemi/parser.py
+++ gersemi/parser.py
@@ -1,12 +1,19 @@
 import os
-import lark
+from lark import Lark, Token
+from lark.visitors import Transformer
+
+
+class DowncaseIdentifiers(Transformer):
+    def IDENTIFIER(self, token):
+        return Token("IDENTIFIER", token.lower())
 
 
 def create_parser():
     this_file_dir = os.path.dirname(os.path.realpath(__file__))
-    return lark.Lark.open(
+    return Lark.open(
         grammar_filename=os.path.join(this_file_dir, "cmake.lark"),
         parser="lalr",
         propagate_positions=True,
         maybe_placeholders=False,
+        transformer=DowncaseIdentifiers(),
     )
