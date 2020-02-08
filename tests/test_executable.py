934271c186828e45bdfec46d14adfe08079acee2
Blank Spruce <32396809+BlankSpruce@users.noreply.github.com>
2020-02-08 12:48:36 +0100

    Tested executable with input from stdin

--- tests/test_executable.py
+++ tests/test_executable.py
@@ -5,8 +5,8 @@ import subprocess
 import tempfile
 
 
-def gersemi(*args):
-    return subprocess.run(["gersemi", *args], check=False)
+def gersemi(*gersemi_args, **subprocess_kwargs):
+    return subprocess.run(["gersemi", *gersemi_args], check=False, **subprocess_kwargs)
 
 
 THIS_FILE_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -28,20 +28,57 @@ def temporary_copy(original):
         os.remove(temp_path)
 
 
+def assert_success(*args, **kwargs):
+    assert gersemi(*args, **kwargs).returncode == 0
+
+
+def assert_fail(*args, **kwargs):
+    assert gersemi(*args, **kwargs).returncode == 1
+
+
 def test_help_just_works():
-    assert gersemi("--help").returncode == 0
+    assert_success("--help")
 
 
 def test_check_on_formatted_file_should_return_zero():
-    assert gersemi("--check", case("formatted_file.cmake")).returncode == 0
+    assert_success("--check", case("formatted_file.cmake"))
 
 
 def test_check_on_not_formatted_file_should_return_one():
-    assert gersemi("--check", case("not_formatted_file.cmake")).returncode == 1
+    assert_fail("--check", case("not_formatted_file.cmake"))
 
 
 def test_format_file_in_place():
     with temporary_copy(case("not_formatted_file.cmake")) as copy:
-        assert gersemi("--check", copy).returncode == 1
+        assert_fail("--check", copy)
         gersemi("--in-place", copy)
-        assert gersemi("--check", copy).returncode == 0
+        assert_success("--check", copy)
+
+
+def test_check_formatted_input_from_stdin():
+    inp = """set(FOO BAR)
+"""
+    assert_success("--check", input=inp, text=True)
+
+
+def test_check_not_formatted_input_from_stdin():
+    inp = """set(FOO BAR)"""  # missing newline at the end
+    assert_fail("--check", input=inp, text=True)
+
+
+def test_format_formatted_input_from_stdin():
+    inp = """set(FOO BAR)
+"""
+    completed_process = gersemi(input=inp, text=True, capture_output=True)
+    assert completed_process.returncode == 0
+    assert completed_process.stdout == inp
+    assert completed_process.stderr == ""
+
+
+def test_format_not_formatted_input_from_stdin():
+    inp = "set(FOO BAR)"  # missing newline at the end
+
+    completed_process = gersemi(input=inp, text=True, capture_output=True)
+    assert completed_process.returncode == 0
+    assert completed_process.stdout == inp + "\n"
+    assert completed_process.stderr == ""
