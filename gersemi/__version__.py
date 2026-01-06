3fc9a95a9b58614444e3582997beda5e4d5646bf
Blank Spruce <32396809+BlankSpruce@users.noreply.github.com>
2026-01-06 17:39:10 +0100

    Migrate from setup.py to pyproject.toml

--- gersemi/__version__.py
+++ gersemi/__version__.py
@@ -1,7 +1,5 @@
+import importlib.metadata
+
 __author__ = "Blank Spruce"
-__author_email__ = "blankspruce@protonmail.com"
-__description__ = "A formatter to make your CMake code the real treasure"
-__license__ = "MPL 2.0"
 __title__ = "gersemi"
-__url__ = "https://github.com/BlankSpruce/gersemi"
-__version__ = "0.25.0"
+__version__ = importlib.metadata.metadata(__title__)["Version"]
