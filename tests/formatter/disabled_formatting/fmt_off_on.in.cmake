5f1e611c1c18fcb4810bd70745418efb171fde54
Philip Nelson <philipnelson5@gmail.com>
2026-04-09 09:27:33 -0600

    Support other kinds of fencing comments that disable formatting
    The supported pairs are:
    - `# gersemi: off`      / `# gersemi: on`
    - `# cmake-format: off` / `# cmake-format: on`
    - `# fmt: off`          / `# fmt: on`

---
+++ tests/formatter/disabled_formatting/fmt_off_on.in.cmake
@@ -0,0 +1,70 @@
+# This is formatted
+set(FOO BAR)    
+
+set(FOO    BAR   )   
+
+set(    FOO    BAR   )
+
+     set(FOO  BAR)   
+
+# This isn't formatted
+# fmt: off
+set(FOO BAR)    
+
+set(FOO    BAR   )   
+
+set(    FOO    BAR   )
+
+     set(FOO  BAR)   
+# fmt: on
+
+# In blocks similarly
+if(TRUE)
+# This is formatted
+set(FOO BAR)    
+
+set(FOO    BAR   )   
+
+set(    FOO    BAR   )
+
+     set(FOO  BAR)   
+
+# This isn't formatted
+# fmt: off
+set(FOO BAR)    
+
+set(FOO    BAR   )   
+
+set(    FOO    BAR   )
+
+     set(FOO  BAR)   
+
+# nested control block inside disabled formatting block
+ if (TRUE)
+ set(FOO    BAR)
+ endif()
+
+# fmt: on
+
+# and in nested blocks
+if(TRUE)
+# fmt: off
+set(FOO BAR)    
+
+set(FOO    BAR   )   
+
+set(    FOO    BAR   )
+
+     set(FOO  BAR)   
+# fmt: on
+
+set(FOO BAR)    
+
+set(FOO    BAR   )   
+
+set(    FOO    BAR   )
+
+     set(FOO  BAR)   
+endif()
+
+endif()
